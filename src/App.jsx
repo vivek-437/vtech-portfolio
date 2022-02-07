cf1ba02eb06c392b0c9563cc245f7426c44bf68e
vivek-437 <vievkgupta2742003@gmail.com>
2022-02-07 20:20:38 +0530

    nav issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,35 @@
-import React from 'react';
-import {Route,Switch} from 'react-router-dom';
-import Contact from './components/content/Contact';
-import Project from './components/content/Project';
-import Skill from './components/content/Skill';
-import Education from './components/content/Education';
-import Home from './components/content/Home';
-import LeftNavBar from './components/nav/Leftnav';
-import Footer from './components/footer/Footer';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Contact from "./components/content/Contact";
+import Project from "./components/content/Project";
+import Skill from "./components/content/Skill";
+import Education from "./components/content/Education";
+import Home from "./components/content/Home";
+import LeftNavBar from "./components/nav/Leftnav";
+import Footer from "./components/footer/Footer";
 // css
-import './components/footer/footer.css';
-import './components/content/contact.css';
-import './components/content/home.css';
-import './components/content/skill.css';
-import './components/content/education.css';
-import './components/content/project.css'
+import "./components/footer/footer.css";
+import "./components/content/contact.css";
+import "./components/content/home.css";
+import "./components/content/skill.css";
+import "./components/content/education.css";
+import "./components/content/project.css";
 // img
 
-
-const App=()=> {
+const App = () => {
   return (
-  <>
-   <LeftNavBar/>
-  <Switch>
-    <Route exact path='/vtech-portfolio/' component={Home} />
-    <Route  path='/vtech-portfolio/project' component={Project} />
-    <Route  path='/vtech-portfolio/contact' component={Contact} />
-    <Route  path='/vtech-portfolio/education' component={Education} />
-    <Route  path='/vtech-portfolio/skill' component={Skill} />
-  </Switch>
-  <Footer/>
-
-  </>
-    )
+    <>
+      <LeftNavBar />
+      <Switch>
+        <Route exact path="/vtech-portfolio/" component={Home} />
+        <Route exact path="/vtech-portfolio/project" component={Project} />
+        <Route exact path="/vtech-portfolio/contact" component={Contact} />
+        <Route exact path="/vtech-portfolio/education" component={Education} />
+        <Route exact path="/vtech-portfolio/skill" component={Skill} />
+      </Switch>
+      <Footer />
+    </>
+  );
 };
 
 export default App;

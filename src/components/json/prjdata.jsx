56b1d5538ed9c4788eb9a7213c6fb1ee2df9c2b1
vivek-437 <vievkgupta2742003@gmail.com>
2023-05-09 23:26:39 +0530

    add new project

--- src/components/json/prjdata.jsx
+++ src/components/json/prjdata.jsx
@@ -5,6 +5,7 @@ import Imrg4 from '../../img/prjimg/temperature.jpg';
 import Imrg5 from "../../img/prjimg/portfolio.jpg";
 import Imrg6 from "../../img/prjimg/Shopping-website.jpg";
 import Imrg7 from "../../img/prjimg/netflix-clone.jpg";
+import Imrg8 from "../../img/prjimg/music player.jpeg";
 
 
 const Prjdata=[
@@ -13,7 +14,7 @@ const Prjdata=[
         title:"Food order-website",
         title_link:"https://vtechfoodorder.000webhostapp.com/",
         data:"It was a great Experience while dealing with database, fetching data from database and using sessions .throughout this project my skills are improved",
-        link:"https://github.com/vivek-437/Php-Mysql/tree/main/Shopping%20website",
+        link:"https://github.com/vivek-437",
     },
     
     {
@@ -21,42 +22,49 @@ const Prjdata=[
         title:"Netflix-Clone",
         title_link:"https://vivek-437.github.io/Netflix-Clone/",
         data:"While working on netflix I am so excited to paly with APIs and I learned how to fetch data from APIs.",
-        link:"https://github.com/vivek-437/Netflix-Clone",
+        link:"https://github.com/vivek-437",
     },
     {
         imsrc:Imrg5,
         title:"my-website",
         data:"Working on my own portfolio I learned new css properties such as animation and creating different components make its easy..",
         title_link:"https://vivek-437.github.io/vtech-portfolio/",
-        link:"https://github.com/vivek-437/vtech-portfolio",
+        link:"https://github.com/vivek-437",
     }
     ,
+    
+    {
+        imsrc:Imrg8,
+        title:"expression recognition music player",
+        data:"The application is designed to capture a facial image and use facial recognition method to suggest a song based on the features of the individual's face..",
+        link:"https://github.com/vivek-437",
+    },
     {
         imsrc:Imrg1,
         title:"x zero",
         data:"I am always exicted to play my own game so I  tried to programmed small game. I personally enjoy playing it..",
         title_link:"https://vivek-437.github.io/x-and-zero/",
-        link:"https://github.com/vivek-437/javascript/tree/main/project/x%20and%200",
+        link:"https://github.com/vivek-437",
     },
     {
         imsrc:Imrg2,
         title:"clock",
         data:"It was difficult  me to find how many time it take to cover 360deg(60 second). Here I learn some thing new such as animation and all.. ",
         title_link:"https://vivek-437.github.io/realtime_clock/",
-        link:"https://github.com/vivek-437/javascript/tree/main/project/Clock",
+        link:"https://github.com/vivek-437",
     },
     
     {
         imsrc:Imrg4,
         title:"temperature converter",
         data:"My younger brother asked me how many units of temperature are there I said two he was like😂 there is three. so I programmed this to calculate temperature unit..",
-        link:"https://github.com/vivek-437/javascript/tree/main/project/temperature%20converter",
+        link:"https://github.com/vivek-437",
     },
     {
         imsrc:Imrg3,
         title:"music player",
         data:"It was quite challenging for me to make music player. While doing it  I got stucked between,which i learned and correct it by learning from some youtube channel and other website..",
-        link:"https://github.com/vivek-437/javascript/tree/main/project/music%20player",
+        link:"https://github.com/vivek-437",
     }
 ]
 
